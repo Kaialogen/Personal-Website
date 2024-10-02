63dcbde3fb50c06f09e445111bd6aa61c8473c1f
Kaialogen <kaialogen@protonmail.com>
2024-10-02 19:26:17 +0100

    cleanup

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,6 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
 
 export default function Home({ allPostsData }) {
   return (
@@ -19,7 +9,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Ka1al0gen, a passionate student currently immersing myself in the fascinating world of Cybersecurity. I have a particular interest in ethical hacking, a field that allows me to use my skills to improve security and protect information. I believe that in our increasingly digital world, the importance of cybersecurity cannot be overstated. Through my studies, I've gained a deep understanding of various security protocols, encryption techniques, and how to identify and exploit vulnerabilities ethically. But my journey is far from over. I'm continuously learning, experimenting, and staying updated with the latest trends and advancements in the field. My goal is to not only become an expert in ethical hacking and cybersecurity but also to contribute to making the digital world a safer place for everyone. Join me as I share my experiences, insights, and tips on this blog. Welcome to my cybersecurity journey!</p>
+        <p></p>
       </section>
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,6 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import { getSortedPostsData } from '../lib/posts';
-import Date from '../components/date';
-
-// Fetches the sorted post data for static site generation
-export async function getStaticProps() {
-  // Fetch all post data
-  const allPostsData = getSortedPostsData();
-  
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
 
 // Home component that renders the homepage
 export default function Home({ allPostsData }) {
@@ -29,26 +14,6 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         {/* Introduction paragraph */}
         <p>Hello, I am Ka1al0gen. I'm a student interesting in ethical hacking. You can find more on my <a href="https://github.com/Kaialogen"> Github</a>.</p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        {/* Blog posts header */}
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        
-        {/* List of blog posts */}
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {/* Each post is a link to its individual page */}
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              
-              {/* Displaying the date of the post */}
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
 
       <footer>
